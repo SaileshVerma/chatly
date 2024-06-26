4980198097a97e6f4b974d3e1a95a3722a0db951
sailesh <vermasailesh271@gmail.com>
2024-06-26 22:11:00 +0530

    inital commit

---
+++ lib/routes/app_router.dart
@@ -0,0 +1,46 @@
+import 'package:chatly/screens/chat_screen/chat_screen.dart';
+import 'package:chatly/screens/home_screen/home_screen.dart';
+import 'package:chatly/screens/login_screen/login_screen.dart';
+import 'package:chatly/screens/signup_screen/signup_screen.dart';
+import 'package:chatly/utils/constants/routes.dart';
+import 'package:flutter/material.dart';
+
+class AppRouter {
+  static Route onGenerateRoute(RouteSettings settings) {
+    Widget page;
+    int transitionDuration = 300;
+    int reverseTransitionDuration = 300;
+
+    switch (settings.name) {
+      case (Routes.login):
+        page = const LoginScreen();
+        break;
+
+      case (Routes.signUp):
+        page = const SignUpScreen();
+        break;
+
+      // case (Routes.root):
+      //   page = const SplashScreen();
+      //   break;
+
+      case (Routes.home):
+        page = const HomeScreen();
+        break;
+
+      default:
+        page = const ChatScreen();
+        break;
+    }
+
+    return PageRouteBuilder(
+      transitionDuration: Duration(milliseconds: transitionDuration),
+      reverseTransitionDuration: Duration(
+        milliseconds: reverseTransitionDuration,
+      ),
+      pageBuilder: (context, animation, secondaryAnimation) {
+        return page;
+      },
+    );
+  }
+}
---
+++ lib/utils/constants/routes.dart
@@ -0,0 +1,9 @@
+// All the route names are defined here
+
+class Routes {
+  static const String root = '/';
+  static const String home = '/home';
+  static const String chat = '/chat';
+  static const String login = '/login';
+  static const String signUp = '/signUp';
+}
